d8158b8b6bf8ae54f26bc9530687639ee841471c
Hendi Santika <hendisantika@yahoo.co.id>
2018-11-15 06:25:44 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootquotes/SpringbootQuotesApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootQuotesApplication.class, args);
+    }
+}
